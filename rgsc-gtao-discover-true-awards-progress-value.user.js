4cbd84b40c98c9bfa0d20eef623e07bbc4812d71
PLTytus <rafal.tytus98@gmail.com>
2024-07-01 08:53:56 +0200

    1.0.2 - number_format

--- rgsc-gtao-discover-true-awards-progress-value.user.js
+++ rgsc-gtao-discover-true-awards-progress-value.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            RGSC-GTAO Discover True Awards Progress Value
 // @author          PLTytus
-// @version         1.0.1
+// @version         1.0.2
 // @namespace       http://gtaweb.eu/tampermonkey
 // @downloadURL     https://github.com/PLTytus/tampermonkey-rgsc-gtao-discover-true-awards-progress-value/raw/master/rgsc-discover-true-awards-progress-value.user.js
 // @updateURL       https://github.com/PLTytus/tampermonkey-rgsc-gtao-discover-true-awards-progress-value/raw/master/rgsc-discover-true-awards-progress-value.meta.js
@@ -12,13 +12,35 @@
 (function() {
     'use strict';
 
+    function number_format(number, decimals, dec_point, thousands_sep){
+        number = (number + '').replace(/[^0-9+\-Ee.]/g, '');
+        let n = !isFinite(+number) ? 0 : +number,
+            prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+            sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+            dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+            s = '',
+            toFixedFix = function(n, prec){
+                let k = Math.pow(10, prec);
+                return '' + Math.round(n * k) / k;
+            };
+        s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+        if(s[0].length > 3){
+            s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+        }
+        if((s[1] || '').length < prec){
+            s[1] = s[1] || '';
+            s[1] += new Array(prec - s[1].length + 1).join('0');
+        }
+        return s.join(dec);
+    }
+
     let interval = setInterval(() => {
         let awards = document.querySelectorAll(".gtavContentArea");
 
         if(awards.length){
             awards.forEach(award => {
                 let info = document.createElement("div");
-                info.innerText = `${award.dataset.value} / ${award.dataset.target}`;
+                info.innerText = `${number_format(award.dataset.value, 0, ',', '.')} / ${number_format(award.dataset.target, 0, ',', '.')}`;
                 info.style.position = 'absolute';
                 info.style.top = '3px';
                 info.style.left = '3px';
