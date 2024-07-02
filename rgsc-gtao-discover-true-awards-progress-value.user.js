fa3a9dd6daa84435142e2faaef145f36177c4c08
bitoblackmane <92067814+bitoblackmane@users.noreply.github.com>
2024-07-02 00:35:58 -0500

    Update rgsc-gtao-discover-true-awards-progress-value.user.js

--- rgsc-gtao-discover-true-awards-progress-value.user.js
+++ rgsc-gtao-discover-true-awards-progress-value.user.js
@@ -5,8 +5,8 @@
 // @namespace       http://gtaweb.eu/tampermonkey
 // @downloadURL     https://github.com/PLTytus/tampermonkey-rgsc-gtao-discover-true-awards-progress-value/raw/master/rgsc-discover-true-awards-progress-value.user.js
 // @updateURL       https://github.com/PLTytus/tampermonkey-rgsc-gtao-discover-true-awards-progress-value/raw/master/rgsc-discover-true-awards-progress-value.meta.js
-// @match           https://socialclub.rockstargames.com/games/gtav/pc/career/awards
-// @match           https://socialclub.rockstargames.com/games/gtav/pc/career/awards/*
+// @match           https://socialclub.rockstargames.com/games/gtav/ps5/career/awards
+// @match           https://socialclub.rockstargames.com/games/gtav/ps5/career/awards/*
 // ==/UserScript==
 
 (function() {
